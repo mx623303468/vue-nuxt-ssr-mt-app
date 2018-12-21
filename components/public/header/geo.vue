41bcd7ba29e1c6d33512a354b3276b523eeb16f7
mx623303468 <623303468@qq.com>
2018-12-21 18:03:18 +0800

    引入css，创建头部组件目录

---
+++ components/public/header/geo.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="m-geo">
+    <i class="el-icon-location"/>深圳市
+    <nuxt-link
+      class="changeCity"
+      to="/changeCity"
+    >
+      切换城市
+    </nuxt-link>
+    [广州 香港 惠州]
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="sass" scoped>
+</style>
---
+++ components/public/header/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="m-header">
+    <Geo/>
+  </div>
+</template>
+
+<script>
+import Geo from './geo'
+export default {
+  components: {
+    Geo
+  }
+}
+</script>
